aaa903a70b02de3b4c918a5821117781bf078cbc
Bilal Asif <bilalasif435@hotmail.com>
2020-07-09 17:43:06 +0500

    Updated Lead model to have owner field, made migrations, added actions to change state when error occurs in getting and creating leads

--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,9 +1,16 @@
-import { CREATE_MESSAGE } from './types'
+import { CREATE_MESSAGE, GET_ERRORS } from './types'
 
 // Create Message
 export const createMessage = msg => {
     return {
         type: CREATE_MESSAGE,
         payload: msg
+    };
+}
+export const returnErrors = (msg, status) => {
+    console.log(msg, status)
+    return {
+        type: GET_ERRORS,
+        payload: { msg, status }
     }
 }
