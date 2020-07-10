f16b601fb9f305e427f8545846523be4f76f7343
Bilal Asif <bilalasif435@hotmail.com>
2020-07-10 16:59:26 +0500

    Added auth reducer and actions to perform authentication as well as managin components to be rendered based on authentication status of user.

---
+++ leadmanager/frontend/src/reducers/auth.js
@@ -0,0 +1,34 @@
+import { USER_LOADING, USER_LOADED, AUTH_ERROR } from '../actions/types'
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+}
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case USER_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            }
+        case AUTH_ERROR:
+            localStorage.removeItem('token')
+            return {
+                ...state,
+                token: null,
+                user: null,
+                isLoading: false,
+                isAuthenticated: false
+            }
+        default:
+            return state
+    }
+}
